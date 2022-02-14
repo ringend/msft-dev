81cb1d10c4fe94f8f16d68928f9229e8d28ba0ea
Deron Ringen <deron.ringen@microsfot.com>
2022-02-14 09:10:16 -0600

    9:10am

--- vnet/build.sh
+++ vnet/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-az group create --location centralus --resource-group bicep-test-rg
-az deployment group create -g bicep-test-rg  --template-file test-1.bicep 
-
+#az group create --location centralus --resource-group bicep-test-rg
+#az deployment group create -g bicep-test-rg  --template-file test-1.bicep 
+ls
